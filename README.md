0128f7bee0c357bd08c40b96a193e60c21fb70e2
mark <sheme4ko@mail.ru>
2015-09-29 00:02:00 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+my-sfp.loc
+==========
+
+A Symfony project created on September 28, 2015, 10:28 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark
+ * Date: 28.09.15
+ * Time: 22:37
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
